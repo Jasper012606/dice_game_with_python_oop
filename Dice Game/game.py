6ac2fbfb1ee6147277a574e93c0304c0c7877e48
Jasper <japeemga@gmail.com>
2025-06-24 04:36:47 +0800

    make a function that will run the game itself, while the highest score among the players is still less than the max score

--- Dice Game/game.py
+++ Dice Game/game.py
@@ -6,7 +6,14 @@ class Game(Dice):
         self.number_of_players = number_of_players
         self.max_score = max_score
         self.player_scores = [0 for _ in range(self.number_of_players)]
-
+        
+    def run(self, roll):
+        while max(self.player_scores) < self.max_score:
+            for player_idx in range(self.number_of_players):
+                print("\nPlayer number", player_idx + 1, "turn has just started!")
+                print("Your total score is:", self.player_scores[player_idx], "\n")
+                current_score = 0
+        
 
 
 
