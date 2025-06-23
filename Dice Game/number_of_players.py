b5a9b638d8b37992b5967908a9b6ef5fa2d354e3
Jasper <japeemga@gmail.com>
2025-06-24 02:38:59 +0800

    Check if the input is valid

--- Dice Game/number_of_players.py
+++ Dice Game/number_of_players.py
@@ -1,3 +1,14 @@
 class NumberOfPlayers:
     def __init__(self, number_of_players):
         self.number_of_players = number_of_players
+        
+    def check_if_valid(self):
+            if self.number_of_players.isdigit():
+                self.number_of_players = int(self.number_of_players)
+                if 2 <= self.number_of_players <= 4:
+                    print(f"Number of players: {self.number_of_players}")
+                    return self.number_of_players
+                else:
+                    print("Players must be between 2 and 4.")
+            else:
+                print("Input invalid, please enter a number between 2 and 4.")
