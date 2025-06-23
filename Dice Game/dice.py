79237690bc21af4e1441ef7a34e893bf80e08948
Jasper <japeemga@gmail.com>
2025-06-24 04:33:34 +0800

    removed the checking part of the code

--- Dice Game/dice.py
+++ Dice Game/dice.py
@@ -6,7 +6,3 @@ class Dice:
     def roll(self, min_value=1, max_value=6):
         roll = random.randint(min_value, max_value)
         return roll
-    
-value = Dice()
-print(value.roll())  # Example usage, prints a random number between 1 and 6
-        
